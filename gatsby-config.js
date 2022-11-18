2d7a490d2712a99a27847dde6b96fe1424699d5e
QuiteAFancyEmerald <46467239+QuiteAFancyEmerald@users.noreply.github.com>
2022-11-18 12:04:04 -0600

    few more changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,8 @@
 module.exports = {
+    siteMetadata: {
+        title: `Titanium Network | SDK Documentation`,
+        siteUrl: `https://docs.titaniumnetwork.org`,
+        description: `The official documentations site for Titanium Network, an organization dedicated innovating web proxy technologies. Made with Docz.`,
+    },
     plugins: ['gatsby-theme-docz']
 }
--- src/gatsby-theme-docz/wrapper.js
+++ src/gatsby-theme-docz/wrapper.js
@@ -4,11 +4,11 @@ import { Helmet } from 'react-helmet-async'
 const Wrapper = ({ children, doc }) => <React.Fragment>
     <Helmet>
         <meta charSet="utf-8" />
-        <meta name="og:title" property="og:title" content="Titanium Network | SDK Documentation"/>
+        <meta name="og:title" property="og:title" content="Titanium Network"/>
         <title>Titanium Network | SDK Documentation</title>
         <link rel="icon" type="image/png" href="https://titaniumnetwork.org/logo.png"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no"/>
-        <meta name="description" content="The official site for Titanium Network, an organization dedicated to providing free access to information by innovating web proxy technologies and bypassing censorship."/>
+        <meta name="description" content="The official documentations site for Titanium Network, an organization dedicated innovating web proxy technologies. Made with Docz."/>
         <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#434c5e"/>
         <meta name="keywords" content="proxy, web proxy, unblock websites, unblock chromebook, free web proxy, proxy list, proxy sites, un block chromebook, online proxy, proxy server, proxysite, proxy youtube, bypass securly, bypass iboss, bypass lightspeed filter, holy unblocker, chromebooks, titanium network, unblock youtube, youtube proxy, unblocked youtube, youtube unblocked, Titanium Network, Hypertabs, Securly, iboss, Relay, Lightspeed, Unblock, Bypass, Chromebooks, Alloy Proxy"/>
         <meta name="googlebot" content="index, follow, snippet" />
