ee20da67eee930b50bc82af3244af50c437e9459
QuiteAFancyEmerald <46467239+QuiteAFancyEmerald@users.noreply.github.com>
2024-01-16 06:39:28 -0800

    SEO Changes (Homepage)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,57 +1,50 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const lightCodeTheme = require("prism-react-renderer").themes.github;
+const darkCodeTheme = require("prism-react-renderer").themes.vsDark;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Docs",
+  title: 'Docs',
   tagline:
-    "The official documentations site for Titanium Network, an organization dedicated innovating web proxy technologies. Made with Docusaurus.",
-  url: "https://docs.titaniumnetwork.org",
-  baseUrl: "/",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "https://titaniumnetwork.org/logo.png",
+    'The official documentation for Titanium Network, an organization dedicated to providing services related to bypassing internet censorship. Our projects expect to provide users, and itself, with a less restrictive browsing experience. Here we provide advanced web proxy services through the progression of web proxy technologies.',
+  url: 'https://docs.titaniumnetwork.org',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'https://titaniumnetwork.org/logo.png',
 
-  organizationName: "titaniumnetwork-dev",
-  projectName: "Oxide-Docs",
+  organizationName: 'titaniumnetwork-dev',
+  projectName: 'Oxide-Docs',
   i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
+    defaultLocale: 'en',
+    locales: ['en'],
   },
 
   themes: [
     [
-      require.resolve("@easyops-cn/docusaurus-search-local"),
+      require.resolve('@easyops-cn/docusaurus-search-local'),
       {
         indexBlog: false,
-        docsRouteBasePath: "/",
+        docsRouteBasePath: '/',
       },
     ],
   ],
 
   presets: [
     [
-      "classic",
+      'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarPath: require.resolve('./sidebars.js'),
           editUrl:
-            "https://github.com/titaniumnetwork-dev/Oxide-Docs/tree/master/docs/",
-          routeBasePath: "/",
+            'https://github.com/titaniumnetwork-dev/Oxide-Docs/tree/master/docs/',
+          routeBasePath: '/',
         },
       }),
     ],
-  ],
-
-  scripts: [
-    {
-      src: "https://titaniumnetwork.org/seo.json",
-      type: "application/ld+json",
-    },
   ],
 
   themeConfig:
@@ -59,28 +52,200 @@ const config = {
     ({
       metadata: [
         {
-          name: "keywords",
+          name: 'keywords',
           content:
-            "proxy, web proxy, unblock websites, unblock chromebook, free web proxy, proxy list, proxy sites, un block chromebook, online proxy, proxy server, proxysite, proxy youtube, bypass securly, bypass iboss, bypass lightspeed filter, holy unblocker, chromebooks, titanium network, unblock youtube, youtube proxy, unblocked youtube, youtube unblocked, Titanium Network, Hypertabs, Securly, iboss, Relay, Lightspeed, Unblock, Bypass, Chromebooks, Alloy Proxy",
+            'proxy, web proxy, unblock websites, unblock chromebook, free web proxy, proxy list, proxy sites, un block chromebook, online proxy, proxy server, proxysite, proxy youtube, bypass securly, bypass iboss, bypass lightspeed filter, holy unblocker, chromebooks, titanium network, unblock youtube, youtube proxy, unblocked youtube, youtube unblocked, Titanium Network, Hypertabs, Securly, iboss, Relay, Lightspeed, Unblock, Bypass, Chromebooks, Alloy Proxy',
         },
-        { name: "googlebot", content: "index, follow, snippet" },
+        { name: 'googlebot', content: 'index, follow, snippet' },
+      ],
+      headTags: [
+        {
+          tagName: 'link',
+          attributes: {
+            rel: 'preconnect',
+            href: 'https://titaniumnetwork.org',
+          },
+        },
+
+        {
+          tagName: 'script',
+          attributes: {
+            type: 'application/ld+json',
+          },
+          innerHTML: JSON.stringify({
+            '@context': 'https://schema.org',
+            '@type': 'Organization',
+            'name': 'Titanium Network',
+            'alternateName': 'Titanium Network (Documentation)',
+            'url': 'https://docs.titaniumnetwork.org',
+            'logo': 'https://titaniumnetwork.org/logo.png',
+            'sameAs': [
+              'https://github.com/titaniumnetwork-dev',
+              'https://twitter.com/titaniumnetdev',
+              'https://www.youtube.com/channel/UC6LaREFvs9L72SK1s2PcxNg',
+              'https://holyubofficial.net'
+            ]
+          })
+        }
+        ,
+        
       ],
       colorMode: {
-        defaultMode: "dark",
+        defaultMode: 'dark',
+        disableSwitch: false,
+        respectPrefersColorScheme: false,
       },
       navbar: {
-        title: "Titanium Network // Documentation",
+        style: "dark",
+        title: 'Titanium Network // Documentation',
         logo: {
-          alt: "TitaniumNetwork",
-          src: "https://titaniumnetwork.org/logo.png",
+          alt: 'TitaniumNetwork',
+          src: 'https://titaniumnetwork.org/logo.png',
         },
         items: [
           {
-            href: "https://github.com/titaniumnetwork-dev/Oxide-Docs",
-            label: "GitHub",
-            position: "right",
+            href: 'https://titaniumnetwork.org',
+            label: 'Official Site',
+            position: 'right',
+          },
+          {
+            href: 'https://discord.gg/unblock',
+            label: 'Discord',
+            position: 'right',
+          },
+          {
+            href: 'https://www.youtube.com/channel/UC6LaREFvs9L72SK1s2PcxNg',
+            label: 'YouTube',
+            position: 'right',
+          },
+          {
+            href: 'https://github.com/titaniumnetwork-dev/Oxide-Docs',
+            label: 'GitHub',
+            position: 'right',
           },
         ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Documentation",
+            items: [
+              {
+                label: "Get Started",
+                to: "/services",
+              },
+              {
+                label: "Guides",
+                to: "/category/guides",
+              },
+              {
+                label: "Web Proxies",
+                to: "/category/proxies",
+              },
+              {
+                label: "Exploits",
+                to: "/services/kajigs",
+              },
+            ],
+          },
+          {
+            title: "Services",
+            items: [
+              {
+                label: "Anura OS",
+                href: "https://anura.pro",
+              },
+              {
+                label: "Holy Unblocker",
+                href: "https://holyubofficial.net",
+              },
+              {
+                label: "Nebula Proxy",
+                href: "https://nebulaproxy.io",
+              },
+              {
+                label: "webretro",
+                href: "https://github.com/BinBashBanana/webretro",
+              },
+              {
+                label: "Flow Works",
+                href: "https://flow-works.me",
+              },
+              {
+                label: "Kazwire",
+                href: "https://kazwire.com",
+              },
+              {
+                label: "Ludicrous",
+                href: "https://ludicrous.icu",
+              },
+              {
+                label: "Radon Games",
+                href: "https://radon.games",
+              },
+              {
+                label: "Totally Science",
+                href: "https://totallyscience.co",
+              },
+            ],
+          },
+          {
+            title: "Archived",
+            items: [
+              {
+                label: "Holy Unblocker (Old)",
+                href: "https://github.com/QuiteAFancyEmerald/Holy-Unblocker",
+              },
+              {
+                label: "Incognito",
+                href: "https://github.com/caracal-js/Incognito",
+              },
+              {
+                label: "Hypertabs",
+                href: "https://github.com/amethystnetwork-dev/Hypertabs",
+              },
+              {
+                label: "Corrosion Proxy",
+                href: "https://github.com/titaniumnetwork-dev/Corrosion",
+              },
+              {
+                label: "Alloy Proxy",
+                href: "https://github.com/titaniumnetwork-dev/alloy",
+              },
+              {
+                label: "PyDodge B",
+                href: "https://github.com/BinBashBanana/PyDodge",
+              },
+              {
+                label: "Greenlime",
+                href: "https://github.com/kol-ton/greenlime",
+              },
+              {
+                label: "P2",
+                href: "https://github.com/titaniumnetwork-dev/titanium-p2",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Mercury Workshop",
+                href: "https://mercurywork.shop",
+              },
+              {
+                label: "Discord",
+                href: "https://discord.gg/unblock",
+              },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/titaniumnetdev",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Titanium Network`,
       },
       prism: {
         theme: lightCodeTheme,
