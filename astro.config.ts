401db83a15ece51fb60648fa43f85e382cd86d55
Ryan Wilson <inbox@ryanwilson.space>
2025-08-29 23:05:50 -0400

    feat: doc updates
    - Document Scramjet and wisp-server-python
    - Officially deprecate UV and recommend Scramjet as the flagship web proxy
    - Resolve relative MD links and prefer them over external GitHub Repo links for projects we have already documented locally
    - Add new frontmatter fields, which create respective buttons for better referencing to external sources for the projects documented
    - Colorize inline codeblocks
    - Switch the default codeblock theme to be tokyo-night

--- astro.config.ts
+++ astro.config.ts
@@ -1,52 +1,74 @@
-import starlight from "@astrojs/starlight";
 import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import rehypeAstroRelativeMarkdownLinks from "astro-rehype-relative-markdown-links";
+import rehypeShiki from "@shikijs/rehype";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://docs.titaniumnetwork.org",
-
-	integrations: [
-		starlight({
-			title: "Titanium Network",
-			favicon: "/favicon.png",
-			social: {
-				github: "https://github.com/titaniumnetwork-dev/Oxide-Docs",
-				discord: "https://discord.gg/unblock",
-			},
-			components: {
-				Head: "./src/components/MetadataHead.astro",
-			},
-			sidebar: [
-				{
-					label: "Guides",
-					autogenerate: { directory: "guides" },
-				},
-				{
-					label: "Services",
-					autogenerate: { directory: "services" },
-				},
-				{
-					label: "Proxies",
-					autogenerate: { directory: "proxies" },
-				},
-				{
-					label: "Technologies",
-					autogenerate: { directory: "technologies" },
-				},
-				{
-					label: "Transports",
-					autogenerate: { directory: "transports" },
-				},
-				{
-					label: "Kajigs",
-					autogenerate: { directory: "kajigs" },
-				},
-			],
-			customCss: [
-				"./src/styles/custom.css",
-				"@fontsource/raleway/400.css",
-				"@fontsource/raleway/600.css",
-			],
-		}),
-	],
+  site: "https://docs.titaniumnetwork.org",
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeAstroRelativeMarkdownLinks,
+        {
+          collectionBase: false,
+        },
+      ],
+      [
+        rehypeShiki,
+        {
+          inline: "tailing-curly-colon",
+          themes: {
+            light: "tokyo-night",
+            dark: "tokyo-night",
+          },
+        },
+      ],
+    ],
+  },
+  integrations: [
+    starlight({
+      title: "Titanium Network",
+      favicon: "/favicon.png",
+      social: {
+        github: "https://github.com/titaniumnetwork-dev/Oxide-Docs",
+        discord: "https://discord.gg/unblock",
+      },
+      components: {
+        Head: "./src/components/MetadataHead.astro",
+        PageTitle: "./src/components/PageTitle.astro",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          autogenerate: { directory: "guides" },
+        },
+        {
+          label: "Services",
+          autogenerate: { directory: "services" },
+        },
+        {
+          label: "Proxies",
+          autogenerate: { directory: "proxies" },
+        },
+        {
+          label: "Technologies",
+          autogenerate: { directory: "technologies" },
+        },
+        {
+          label: "Transports",
+          autogenerate: { directory: "transports" },
+        },
+        {
+          label: "Kajigs",
+          autogenerate: { directory: "kajigs" },
+        },
+      ],
+      customCss: [
+        "./src/styles/custom.css",
+        "@fontsource/raleway/400.css",
+        "@fontsource/raleway/600.css",
+      ],
+    }),
+  ],
 });
