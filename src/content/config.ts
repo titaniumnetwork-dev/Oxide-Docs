b5923b23c713f747fc3a872fee87fac779bacae6
Sophia <193290223+soap-phia@users.noreply.github.com>
2025-09-16 17:58:46 -0400

    aurora, badrecovery, br0ker, caub, cryptosmite, dump-kiosk, extensions, icarus, incognito, index, legacy-kajigs, old-unenroll, omada-dns, oobescape, pencil, protonvpn, rigtools, sh1mmer, sh1ttyoobe, shimboot, terra, unenrollment, versions, wifi - From AerialiteLabs/Aerialite-Docs

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,13 +9,14 @@ export const collections = {
         discord: z.string().optional(),
         "official-link": z.string().optional(),
         kernver: z.number().optional(),
+        unenrollment: z.boolean().optional(),
       }),
     }),
   }),
 };
 
-export async function getkajigs() {
-  const kajigs = ( await getCollection("docs")).filter((doc) => doc.id.startsWith("kajigs/") && doc.id !== "kajigs" && doc.data.kernver !== undefined );
+export async function getkajigs(u: boolean) {
+  const kajigs = ( await getCollection("docs")).filter((doc) => doc.id.startsWith("kajigs/") && doc.data.kernver !== undefined && (!u || doc.data.unenrollment === true));
   const kernver: Record<number, { title: string; slug: string }[]> = {};
   for (const doc of kajigs) {
     const key = doc.data.kernver!;
@@ -24,3 +25,13 @@ export async function getkajigs() {
   }
   return kernver;
 }
+
+export const kernvers = {
+  0: ["Any", "All Versions", "All Versions"],
+  1: ["1", "<v112", "All Versions"],
+  2: "v112-v119",
+  3: "v120-v123",
+  4: "v124-v132",
+  5: "v132-v137",
+  6: "v138+",
+};
